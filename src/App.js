3a9422988f0fa1513388927f456233111f151741
André Ascensão <andremiguelaa@gmail.com>
2019-08-17 18:31:03 +0100

    route title translations

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { BrowserRouter, Switch } from "react-router-dom";
-
 import { I18nProvider, I18n } from "@lingui/react";
 import { t } from "@lingui/macro";
+import slugify from "slugify";
 
 import catalogPt from "locales/pt/messages.js";
 import catalogEn from "locales/en/messages.js";
-
 import "styles/App.scss";
-
+import routes from "routes";
 import { useStateValue } from "state/State";
 import Auth from "utils/Auth";
 import RouteWithTitle from "utils/RouteWithTitle";
-
-import routes from "routes";
-
 import Header from "partials/Header";
 
 export const catalogs = { pt: catalogPt, en: catalogEn };
@@ -23,34 +19,46 @@ const App = () => {
   const [{ settings }] = useStateValue();
   return (
     <I18nProvider language={settings.language} catalogs={catalogs}>
-      <BrowserRouter>
-        <Auth>
-          <Header />
-          <main>
-            <section className="section">
-              <div className="container">
-                <div className="columns">
-                  <I18n>
-                    {({ i18n }) => (
+      <I18n>
+        {({ i18n }) => (
+          <BrowserRouter>
+            <Auth>
+              <Header />
+              <main>
+                <section className="section">
+                  <div className="container">
+                    <div className="columns">
                       <Switch>
                         {routes.map(route => {
-                          let { slug, ...rest } = route;
-                          const title = i18n._(t`${route.title.props.id}`);
-                          rest = {
-                            ...rest,
+                          const title = route.root
+                            ? catalogs[settings.language].messages[
+                                route.title.props.id
+                              ]
+                            : `${i18n._(t`Liga Quiz`)} | ${
+                                catalogs[settings.language].messages[
+                                  route.title.props.id
+                                ]
+                              }`;
+                          const newProps = {
+                            ...route,
                             title
                           };
-                          return <RouteWithTitle key={route.slug} {...rest} />;
+                          return (
+                            <RouteWithTitle
+                              key={slugify(title)}
+                              {...newProps}
+                            />
+                          );
                         })}
                       </Switch>
-                    )}
-                  </I18n>
-                </div>
-              </div>
-            </section>
-          </main>
-        </Auth>
-      </BrowserRouter>
+                    </div>
+                  </div>
+                </section>
+              </main>
+            </Auth>
+          </BrowserRouter>
+        )}
+      </I18n>
     </I18nProvider>
   );
 };
--- src/routes.js
+++ src/routes.js
@@ -11,49 +11,43 @@ import NoMatch from "pages/NoMatch";
 
 export default [
   {
-    slug: "home",
     title: <Trans>Liga Quiz</Trans>,
+    root: true,
     exact: true,
     path: "/",
     component: Home
   },
   {
-    slug: "login",
     title: <Trans>Entrar</Trans>,
     exact: true,
     path: "/login",
     component: Login
   },
   {
-    slug: "recover-password",
     title: <Trans>Recuperar palavra-passe</Trans>,
     exact: true,
     path: "/recover-password",
     component: RecoverPassword
   },
   {
-    slug: "reset-password",
     title: <Trans>Redefinir palavra-passe</Trans>,
     exact: true,
     path: "/reset-password/:token",
     component: ResetPassword
   },
   {
-    slug: "register",
     title: <Trans>Registo</Trans>,
     exact: true,
     path: "/register",
     component: Register
   },
   {
-    slug: "logout",
     title: <Trans>Sair</Trans>,
     exact: true,
     path: "/logout",
     component: Logout
   },
   {
-    slug: "no-match",
     title: <Trans>Página não encontrada</Trans>,
     component: NoMatch
   }
