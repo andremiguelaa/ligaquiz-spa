7ed4a92d14b27f94a38e23b48ae5132230854b30
André Ascensão <andremiguelaa@gmail.com>
2019-08-18 19:53:09 +0100

    avatar upload

--- src/pages/ControlPanel/ControlPanelProfile.js
+++ src/pages/ControlPanel/ControlPanelProfile.js
@@ -6,6 +6,7 @@ import { toast } from 'react-toastify';
 
 import { useStateValue } from 'state/State';
 import ApiRequest from 'utils/ApiRequest';
+import ControlPanelProfileAvatar from 'pages/ControlPanel/ControlPanelProfileAvatar';
 
 const ControlPanelProfile = () => {
   const [{ user }, dispatch] = useStateValue();
@@ -39,7 +40,7 @@ const ControlPanelProfile = () => {
         });
         dispatch({
           type: 'user.patch',
-          payload: newFormData
+          payload: omit(newFormData, ['password', 'password2'])
         });
       })
       .catch(error => {
@@ -53,150 +54,160 @@ const ControlPanelProfile = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="field">
-        <label className="label">
-          <Trans>Nome</Trans>
-        </label>
-        <div className="control has-icons-left">
-          <input
-            type="name"
-            required
-            maxLength={255}
-            className="input"
-            defaultValue={user.name}
-            onChange={event => {
-              setformData({
-                ...formData,
-                name: event.target.value
-              });
-            }}
-          />
-          <span className="icon is-small is-left">
-            <i className="fa fa-user" />
-          </span>
-        </div>
-      </div>
-      <div className="field">
-        <label className="label">
-          <Trans>Apelido</Trans>
-        </label>
-        <div className="control has-icons-left">
-          <input
-            type="surname"
-            required
-            maxLength={255}
-            className="input"
-            defaultValue={user.surname}
-            onChange={event => {
-              setformData({
-                ...formData,
-                surname: event.target.value
-              });
-            }}
-          />
-          <span className="icon is-small is-left">
-            <i className="fa fa-user" />
-          </span>
-        </div>
-      </div>
-      <div className="field">
-        <label className="label">
-          <Trans>E-Mail</Trans>
-        </label>
-        <div className="control has-icons-left">
-          <input
-            type="email"
-            required
-            className={classNames('input', {
-              'is-danger': error && error.data && error.data.email
-            })}
-            defaultValue={user.email}
-            onChange={event => {
-              setformData({
-                ...formData,
-                email: event.target.value
-              });
-            }}
-          />
-          <span className="icon is-small is-left">
-            <i className="fa fa-envelope" />
-          </span>
-        </div>
-        {error &&
-          error.data &&
-          error.data.email &&
-          error.data.email.includes('validation.unique') && (
-            <p className="help is-danger">
-              <Trans>Já existe uma conta com este e-mail.</Trans>
-            </p>
+    <div className="columns">
+      <div className="column is-6">
+        <form onSubmit={handleSubmit}>
+          <div className="field">
+            <label className="label">
+              <Trans>Nome</Trans>
+            </label>
+            <div className="control has-icons-left">
+              <input
+                type="name"
+                required
+                maxLength={255}
+                className="input"
+                defaultValue={user.name}
+                onChange={event => {
+                  setformData({
+                    ...formData,
+                    name: event.target.value
+                  });
+                }}
+              />
+              <span className="icon is-small is-left">
+                <i className="fa fa-user" />
+              </span>
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">
+              <Trans>Apelido</Trans>
+            </label>
+            <div className="control has-icons-left">
+              <input
+                type="surname"
+                required
+                maxLength={255}
+                className="input"
+                defaultValue={user.surname}
+                onChange={event => {
+                  setformData({
+                    ...formData,
+                    surname: event.target.value
+                  });
+                }}
+              />
+              <span className="icon is-small is-left">
+                <i className="fa fa-user" />
+              </span>
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">
+              <Trans>E-Mail</Trans>
+            </label>
+            <div className="control has-icons-left">
+              <input
+                type="email"
+                required
+                className={classNames('input', {
+                  'is-danger': error && error.data && error.data.email
+                })}
+                defaultValue={user.email}
+                onChange={event => {
+                  setformData({
+                    ...formData,
+                    email: event.target.value
+                  });
+                }}
+              />
+              <span className="icon is-small is-left">
+                <i className="fa fa-envelope" />
+              </span>
+            </div>
+            {error &&
+              error.data &&
+              error.data.email &&
+              error.data.email.includes('validation.unique') && (
+                <p className="help is-danger">
+                  <Trans>Já existe uma conta com este e-mail.</Trans>
+                </p>
+              )}
+          </div>
+          <div className="field">
+            <label className="label">
+              <Trans>Palavra-passe</Trans> (
+              <Trans>deixa em branco para não alterar</Trans>)
+            </label>
+            <div className="control has-icons-left">
+              <input
+                type="password"
+                minLength={6}
+                maxLength={255}
+                className="input"
+                autoComplete="new-password"
+                onChange={event => {
+                  setformData({
+                    ...formData,
+                    password: event.target.value
+                  });
+                }}
+              />
+              <span className="icon is-small is-left">
+                <i className="fa fa-key" />
+              </span>
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">
+              <Trans>Confirmação da palavra-passe</Trans>
+            </label>
+            <div className="control has-icons-left">
+              <input
+                type="password"
+                minLength={6}
+                maxLength={255}
+                className={`input ${formData.password2.length &&
+                  formData.password !== formData.password2 &&
+                  'is-danger'}`}
+                onChange={event => {
+                  setformData({
+                    ...formData,
+                    password2: event.target.value
+                  });
+                }}
+              />
+              <span className="icon is-small is-left">
+                <i className="fa fa-key" />
+              </span>
+            </div>
+          </div>
+          {error && error.message !== 'validation_error' && (
+            <div className="field">
+              <p className="help is-danger">
+                <Trans>Erro de servidor. Tenta mais tarde.</Trans>
+              </p>
+            </div>
           )}
+          <div className="field">
+            <div className="control">
+              <button
+                className={`button is-primary ${submitting && 'is-loading'}`}
+                disabled={
+                  submitting || formData.password !== formData.password2
+                }
+              >
+                <Trans>Gravar</Trans>
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-      <div className="field">
-        <label className="label">
-          <Trans>Palavra-passe (deixa em branco para não alterar)</Trans>
-        </label>
-        <div className="control has-icons-left">
-          <input
-            type="password"
-            minLength={6}
-            maxLength={255}
-            className="input"
-            autoComplete="new-password"
-            onChange={event => {
-              setformData({
-                ...formData,
-                password: event.target.value
-              });
-            }}
-          />
-          <span className="icon is-small is-left">
-            <i className="fa fa-key" />
-          </span>
-        </div>
+      <div className="column is-6">
+        <ControlPanelProfileAvatar />
       </div>
-      <div className="field">
-        <label className="label">
-          <Trans>Confirmação da palavra-passe</Trans>
-        </label>
-        <div className="control has-icons-left">
-          <input
-            type="password"
-            minLength={6}
-            maxLength={255}
-            className={`input ${formData.password2.length &&
-              formData.password !== formData.password2 &&
-              'is-danger'}`}
-            onChange={event => {
-              setformData({
-                ...formData,
-                password2: event.target.value
-              });
-            }}
-          />
-          <span className="icon is-small is-left">
-            <i className="fa fa-key" />
-          </span>
-        </div>
-      </div>
-      {error && error.message !== 'validation_error' && (
-        <div className="field">
-          <p className="help is-danger">
-            <Trans>Erro de servidor. Tenta mais tarde.</Trans>
-          </p>
-        </div>
-      )}
-      <div className="field">
-        <div className="control">
-          <button
-            className={`button is-primary ${submitting && 'is-loading'}`}
-            disabled={submitting || formData.password !== formData.password2}
-          >
-            <Trans>Gravar</Trans>
-          </button>
-        </div>
-      </div>
-    </form>
+    </div>
   );
 };
 
---
+++ src/pages/ControlPanel/ControlPanelProfileAvatar.js
@@ -0,0 +1,122 @@
+import React, { useState } from 'react';
+import { Trans } from '@lingui/macro';
+import { toast } from 'react-toastify';
+
+import { useStateValue } from 'state/State';
+import ApiRequest from 'utils/ApiRequest';
+
+const ControlPanelProfileAvatar = () => {
+  const [{ user }, dispatch] = useStateValue();
+  const [formData, setformData] = useState({
+    id: user.id,
+    avatar: null
+  });
+  const [avatar, setAvatar] = useState(user.avatar_url);
+  const [error, setError] = useState(null);
+  const [submitting, setSubmitting] = useState(false);
+
+  const avatarChange = event => {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        setAvatar(reader.result);
+        setformData({
+          ...formData,
+          avatar: reader.result
+        });
+      };
+    }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+    setError(null);
+    ApiRequest.patch('users', formData)
+      .then(() => {
+        setSubmitting(false);
+        toast.success(
+          <Trans>Imagem de perfil actualizada com sucesso.</Trans>,
+          {
+            hideProgressBar: true,
+            closeButton: false
+          }
+        );
+        dispatch({
+          type: 'user.patch',
+          payload: formData
+        });
+      })
+      .catch(error => {
+        try {
+          setError(error.response.data);
+        } catch (error) {
+          setError({ message: 'server_error' });
+        }
+        setSubmitting(false);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="field">
+        <label className="label">
+          <Trans>Imagem de perfil</Trans>
+        </label>
+        <div className="file">
+          <label className="file-label">
+            <input
+              className="file-input"
+              type="file"
+              accept="image/x-png,image/gif,image/jpeg"
+              onChange={event => {
+                avatarChange(event);
+              }}
+            />
+            <span className="file-cta">
+              <span className="file-icon">
+                <i className="fa fa-upload" />
+              </span>
+              <span className="file-label">
+                <Trans>Escolhe uma imagem*</Trans>
+              </span>
+            </span>
+          </label>
+        </div>
+        <p>
+          <small>
+            * <Trans>Máximo 200 KB</Trans>
+          </small>
+        </p>
+        <figure className="avatar">
+          <img src={avatar} alt={user.name} />
+        </figure>
+      </div>
+      {error && (
+        <div className="field">
+          <p className="help is-danger">
+            {error.message === 'validation_error' ? (
+              <Trans>Ficheiro inválido.</Trans>
+            ) : (
+              <Trans>Erro de servidor. Tenta mais tarde.</Trans>
+            )}
+          </p>
+        </div>
+      )}
+      <div className="field">
+        <div className="control">
+          <button
+            className={`button is-primary ${submitting && 'is-loading'}`}
+            disabled={submitting || !formData.avatar}
+          >
+            <Trans>Gravar</Trans>
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default ControlPanelProfileAvatar;
