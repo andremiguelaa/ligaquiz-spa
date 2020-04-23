6292f2027e75d7e5eda46bebdfd0a22aa22ffbd5
André Ascensão <andremiguelaa@gmail.com>
2020-04-23 01:21:13 +0100

    add error util

---
+++ src/utils/Error.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import classes from './Error.module.scss';
+
+export const Error = ({ children }) => (
+  <>
+    <div className={classes.error}>
+      <i className="fa fa-bomb" />
+    </div>
+    <div className={classes.content}>
+        {children}
+    </div>
+  </>
+);
+
+Error.defaultProps = {
+  children: null,
+};
+
+export default Error;
