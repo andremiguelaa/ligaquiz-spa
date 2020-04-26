83ee2baf95779715e5f57545acb642b07a98f7a9
André Ascensão <andremiguelaa@gmail.com>
2020-04-26 04:05:51 +0100

    ranking add wip

--- src/components/ControlPanel/Ranking/Add.js
+++ src/components/ControlPanel/Ranking/Add.js
@@ -2,55 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Trans } from '@lingui/macro';
 
 import Modal from 'utils/Modal';
-
-const individualQuizTypeOptions = (type, render) =>
-  ({
-    cnq: (
-      <Trans key="cnq" render={render}>
-        Campeonato Nacional de Quiz
-      </Trans>
-    ),
-    eqc: (
-      <Trans key="eqc" render={render}>
-        Campeonato Europeu de Quiz
-      </Trans>
-    ),
-    inquizicao: (
-      <Trans key="inquizicao" render={render}>
-        Inquizição
-      </Trans>
-    ),
-    squizzed: (
-      <Trans key="squizzed" render={render}>
-        Squizzed
-      </Trans>
-    ),
-    wqc: (
-      <Trans key="wqc" render={render}>
-        Campeonato Mundial de Quiz
-      </Trans>
-    ),
-    hot_100: (
-      <Trans key="hot_100" render={render}>
-        HOT 100
-      </Trans>
-    ),
-  }[type]);
-
-let monthListOptions = [];
-let currentMonth = new Date().getMonth() + 1;
-let currentYear = new Date().getFullYear();
-while (!(currentYear === 2016 && currentMonth === 10)) {
-  monthListOptions.push(`${currentYear}-${`${currentMonth}`.padStart(2, '0')}`);
-  currentMonth--;
-  if (!currentMonth) {
-    currentMonth = 12;
-    currentYear--;
-  }
-}
+import { individualQuizTypeOptions, monthListOptions } from './Add/options';
 
 const Add = ({ monthList, individualQuizTypes, setPage }) => {
-  const validMonthListOptions = monthListOptions.filter(
+  const validMonthListOptions = monthListOptions().filter(
     (month) => !monthList.includes(month)
   );
 
---
+++ src/components/ControlPanel/Ranking/Add/options.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Trans } from '@lingui/macro';
+
+export const individualQuizTypeOptions = (type, render) =>
+  ({
+    cnq: (
+      <Trans key="cnq" render={render}>
+        Campeonato Nacional de Quiz
+      </Trans>
+    ),
+    eqc: (
+      <Trans key="eqc" render={render}>
+        Campeonato Europeu de Quiz
+      </Trans>
+    ),
+    inquizicao: (
+      <Trans key="inquizicao" render={render}>
+        Inquizição
+      </Trans>
+    ),
+    squizzed: (
+      <Trans key="squizzed" render={render}>
+        Squizzed
+      </Trans>
+    ),
+    wqc: (
+      <Trans key="wqc" render={render}>
+        Campeonato Mundial de Quiz
+      </Trans>
+    ),
+    hot_100: (
+      <Trans key="hot_100" render={render}>
+        HOT 100
+      </Trans>
+    ),
+  }[type]);
+
+export const monthListOptions = () => {
+  let monthListOptions = [];
+  let currentMonth = new Date().getMonth() + 1;
+  let currentYear = new Date().getFullYear();
+  while (!(currentYear === 2016 && currentMonth === 10)) {
+    monthListOptions.push(
+      `${currentYear}-${`${currentMonth}`.padStart(2, '0')}`
+    );
+    currentMonth--;
+    if (!currentMonth) {
+      currentMonth = 12;
+      currentYear--;
+    }
+  }
+  return monthListOptions;
+};
