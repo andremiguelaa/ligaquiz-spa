5bdf41e98a22358691a58b88d41d51df90fdfe60
André Ascensão <andremiguelaa@gmail.com>
2020-05-28 01:07:52 +0100

    refactoring

--- src/views/admin/users/Users.js
+++ src/views/admin/users/Users.js
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom';
 import { Trans } from '@lingui/macro';
 import { I18n } from '@lingui/react';
 import { t } from '@lingui/macro';
-import { toast } from 'react-toastify';
-import DatePicker from 'react-date-picker';
 
 import { useStateValue } from 'state/State';
 import ApiRequest from 'utils/ApiRequest';
@@ -13,20 +11,14 @@ import Loading from 'components/Loading';
 import Error from 'components/Error';
 import PageHeader from 'components/PageHeader';
 import EmptyState from 'components/EmptyState';
-import Modal from 'components/Modal';
+import EditPermissionsModal from './modals/EditPermissionsModal';
 
 import classes from './Users.module.scss';
 
 const Users = () => {
   const [error, setError] = useState(false);
   const [users, setUsers] = useState();
-  const [
-    {
-      user: authUser,
-      settings: { language },
-    },
-  ] = useStateValue();
-  const [patching, setPatching] = useState(false);
+  const [{ user: authUser }] = useStateValue();
   const [userToEdit, setUserToEdit] = useState();
 
   useEffect(() => {
@@ -38,45 +30,6 @@ const Users = () => {
         setError(true);
       });
   }, []);
-
-  const patchUser = (user) => {
-    setPatching(true);
-    let newRoles = Object.entries(user.newRoles).reduce((acc, [key, value]) => {
-      if (value) {
-        acc[key] = value;
-      }
-      return acc;
-    }, {});
-    ApiRequest.patch('users', {
-      id: user.id,
-      roles: newRoles,
-    })
-      .then(
-        ({
-          data: {
-            data: { user },
-          },
-        }) => {
-          setUsers(
-            users.map((item) => {
-              if (item.id !== user.id) {
-                return item;
-              } else {
-                return user;
-              }
-            })
-          );
-          setUserToEdit();
-          toast.success(<Trans>Utilizador actualizado com sucesso.</Trans>);
-        }
-      )
-      .catch(() => {
-        toast.error(<Trans>Não foi possível actualizar o utilizador.</Trans>);
-      })
-      .finally(() => {
-        setPatching(false);
-      });
-  };
 
   if (error) {
     return (
@@ -194,7 +147,7 @@ const Users = () => {
                           <div className="is-pulled-right">
                             <button
                               className="button"
-                              disabled={patching || user.id === authUser.id}
+                              disabled={user.id === authUser.id}
                               onClick={() => {
                                 setUserToEdit({
                                   ...user,
@@ -222,154 +175,11 @@ const Users = () => {
         )}
       </div>
       {userToEdit && (
-        <Modal
-          open
-          title={
-            <Trans>
-              Editar permissões de {`${userToEdit.name} ${userToEdit.surname}`}
-            </Trans>
-          }
-          body={
-            <>
-              <fieldset className="fieldset">
-                <div className="field">
-                  <input
-                    id="admin"
-                    type="checkbox"
-                    className="switch"
-                    value="true"
-                    onClick={(event) => {
-                      setUserToEdit({
-                        ...userToEdit,
-                        newRoles: {
-                          ...userToEdit.newRoles,
-                          admin: event.target.checked,
-                        },
-                      });
-                    }}
-                    defaultChecked={Boolean(userToEdit.newRoles?.admin)}
-                  />
-                  <label for="admin">
-                    <Trans>Administrador</Trans>
-                  </label>
-                </div>
-              </fieldset>
-              <fieldset className="fieldset">
-                <div className="field">
-                  <input
-                    id="regular-player"
-                    type="checkbox"
-                    className="switch"
-                    value="true"
-                    onClick={(event) => {
-                      setUserToEdit({
-                        ...userToEdit,
-                        newRoles: {
-                          ...userToEdit.newRoles,
-                          regular_player: event.target.checked
-                            ? userToEdit.roles.regular_player ||
-                              formatDate(new Date())
-                            : event.target.checked,
-                        },
-                      });
-                    }}
-                    defaultChecked={Boolean(userToEdit.roles?.regular_player)}
-                  />
-                  <label for="regular-player">
-                    <Trans>Jogador regular</Trans>
-                  </label>
-                </div>
-                <div className="field">
-                  <div className="control">
-                    <label className="label">
-                      <Trans>Validade da subscrição</Trans>
-                    </label>
-                    <div className="control has-icons-left">
-                      <DatePicker
-                        disabled={!Boolean(userToEdit.newRoles?.regular_player)}
-                        value={
-                          userToEdit.newRoles?.regular_player
-                            ? new Date(userToEdit.newRoles?.regular_player)
-                            : userToEdit.roles?.regular_player
-                            ? new Date(userToEdit.roles?.regular_player)
-                            : new Date()
-                        }
-                        onChange={(value) => {
-                          setUserToEdit({
-                            ...userToEdit,
-                            newRoles: {
-                              ...userToEdit.newRoles,
-                              regular_player: formatDate(value),
-                            },
-                          });
-                        }}
-                        calendarIcon={null}
-                        clearIcon={null}
-                        format="yyyy-MM-dd"
-                        locale={language}
-                      />
-                      <span className="icon is-small is-left">
-                        <i className="fa fa-calendar" />
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </fieldset>
-              <fieldset className="fieldset">
-                <div className="field">
-                  <input
-                    id="national-ranking-manager"
-                    type="checkbox"
-                    className="switch"
-                    value="true"
-                    onClick={(event) => {
-                      setUserToEdit({
-                        ...userToEdit,
-                        newRoles: {
-                          ...userToEdit.newRoles,
-                          ranking_manager: event.target.checked,
-                        },
-                      });
-                    }}
-                    defaultChecked={Boolean(
-                      userToEdit.newRoles?.ranking_manager
-                    )}
-                  />
-                  <label for="national-ranking-manager">
-                    <Trans>Gestor de Ranking Nacional</Trans>
-                  </label>
-                </div>
-              </fieldset>
-              <fieldset className="fieldset">
-                <div className="field">
-                  <input
-                    id="blocked"
-                    type="checkbox"
-                    className="switch"
-                    value="true"
-                    onClick={(event) => {
-                      setUserToEdit({
-                        ...userToEdit,
-                        newRoles: {
-                          ...userToEdit.newRoles,
-                          blocked: event.target.checked,
-                        },
-                      });
-                    }}
-                    defaultChecked={Boolean(userToEdit.newRoles?.blocked)}
-                  />
-                  <label for="blocked">
-                    <Trans>Bloqueado</Trans>
-                  </label>
-                </div>
-              </fieldset>
-            </>
-          }
-          action={() => {
-            patchUser(userToEdit);
-          }}
-          doingAction={patching}
-          onClose={() => setUserToEdit()}
+        <EditPermissionsModal
+          userToEdit={userToEdit}
+          setUserToEdit={setUserToEdit}
+          users={users}
+          setUsers={setUsers}
         />
       )}
     </>
