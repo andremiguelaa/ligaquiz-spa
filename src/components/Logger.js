cf27deec3697d9f51a0a72e1856fe921ea34d305
André Ascensão <andremiguelaa@gmail.com>
2020-08-19 01:58:59 +0100

    logger fix

--- src/components/Logger.js
+++ src/components/Logger.js
@@ -2,85 +2,92 @@ import { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import ApiRequest from 'utils/ApiRequest';
 
-// Get selected text
-const getSelectionText = () => {
-  var text = '';
-  var activeEl = document.activeElement;
-  var activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
-  if (
-    activeElTagName === 'textarea' ||
-    (activeElTagName === 'input' &&
-      /^(?:text|search|password|tel|url)$/i.test(activeEl.type) &&
-      typeof activeEl.selectionStart === 'number')
-  ) {
-    text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
-  } else if (window.getSelection) {
-    text = window.getSelection().toString();
-  }
-  return text;
-};
-
-// Detect copy
-const keyPress = (e) => {
-  var text = getSelectionText();
-  if (e.ctrlKey || e.metaKey) {
-    if (e.keyCode === 67) {
-      ApiRequest.post(`logs`, {
-        action: `Text copied${text && `: ${text}`}`,
-      });
-    }
-    if (e.keyCode === 88) {
-      ApiRequest.post(`logs`, {
-        action: `Text cut${text && `: ${text}`}`,
-      });
-    }
-  }
-};
-document.onkeydown = keyPress;
-
-// Detect right click
-window.oncontextmenu = function (e) {
-  var text = getSelectionText();
-  ApiRequest.post(`logs`, {
-    action: `Right click on ${e.target.nodeName} element${text && `: ${text}`}`,
-  });
-};
-
-// Detect page unloaded
-window.addEventListener('beforeunload', function () {
-  ApiRequest.post(`logs`, {
-    action: `Page unloaded: ${window.location.href}`,
-  });
-});
-
-// Detect focus in/out
-let hidden, visibilityChange;
-if (typeof document.hidden !== 'undefined') {
-  hidden = 'hidden';
-  visibilityChange = 'visibilitychange';
-} else if (typeof document.msHidden !== 'undefined') {
-  hidden = 'msHidden';
-  visibilityChange = 'msvisibilitychange';
-} else if (typeof document.webkitHidden !== 'undefined') {
-  hidden = 'webkitHidden';
-  visibilityChange = 'webkitvisibilitychange';
-}
-const handleVisibilityChange = () => {
-  if (document[hidden]) {
-    ApiRequest.post(`logs`, {
-      action: `Page focus out: ${window.location.pathname}`,
-    });
-  } else {
-    ApiRequest.post(`logs`, {
-      action: `Page focus in: ${window.location.pathname}`,
-    });
-  }
-};
-document.addEventListener(visibilityChange, handleVisibilityChange, false);
-
 const Logger = () => {
   const history = useHistory();
   const load = useRef();
+
+  useEffect(() => {
+    // Get selected text
+    const getSelectionText = () => {
+      var text = '';
+      var activeEl = document.activeElement;
+      var activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
+      if (
+        activeElTagName === 'textarea' ||
+        (activeElTagName === 'input' &&
+          /^(?:text|search|password|tel|url)$/i.test(activeEl.type) &&
+          typeof activeEl.selectionStart === 'number')
+      ) {
+        text = activeEl.value.slice(
+          activeEl.selectionStart,
+          activeEl.selectionEnd
+        );
+      } else if (window.getSelection) {
+        text = window.getSelection().toString();
+      }
+      return text;
+    };
+
+    // Detect copy
+    const keyPress = (e) => {
+      var text = getSelectionText();
+      if (e.ctrlKey || e.metaKey) {
+        if (e.keyCode === 67) {
+          ApiRequest.post(`logs`, {
+            action: `Text copied${text && `: ${text}`}`,
+          });
+        }
+        if (e.keyCode === 88) {
+          ApiRequest.post(`logs`, {
+            action: `Text cut${text && `: ${text}`}`,
+          });
+        }
+      }
+    };
+    document.onkeydown = keyPress;
+
+    // Detect right click
+    window.oncontextmenu = function (e) {
+      var text = getSelectionText();
+      ApiRequest.post(`logs`, {
+        action: `Right click on ${e.target.nodeName} element${
+          text && `: ${text}`
+        }`,
+      });
+    };
+
+    // Detect page unloaded
+    window.addEventListener('beforeunload', function () {
+      ApiRequest.post(`logs`, {
+        action: `Page unloaded: ${window.location.href}`,
+      });
+    });
+
+    // Detect focus in/out
+    let hidden, visibilityChange;
+    if (typeof document.hidden !== 'undefined') {
+      hidden = 'hidden';
+      visibilityChange = 'visibilitychange';
+    } else if (typeof document.msHidden !== 'undefined') {
+      hidden = 'msHidden';
+      visibilityChange = 'msvisibilitychange';
+    } else if (typeof document.webkitHidden !== 'undefined') {
+      hidden = 'webkitHidden';
+      visibilityChange = 'webkitvisibilitychange';
+    }
+    const handleVisibilityChange = () => {
+      if (document[hidden]) {
+        ApiRequest.post(`logs`, {
+          action: `Page focus out: ${window.location.pathname}`,
+        });
+      } else {
+        ApiRequest.post(`logs`, {
+          action: `Page focus in: ${window.location.pathname}`,
+        });
+      }
+    };
+    document.addEventListener(visibilityChange, handleVisibilityChange, false);
+  }, []);
 
   useEffect(() => {
     if (!load.current) {
