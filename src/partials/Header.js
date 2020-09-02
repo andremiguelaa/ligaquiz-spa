469f6fd4255afdcfddc74c64b85bab6e4a03184b
André Ascensão <andremiguelaa@gmail.com>
2020-09-02 22:57:35 +0100

    seo improvements

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -11,7 +11,7 @@ import logo from 'img/logo.png';
 import logoDark from 'img/logo-dark.png';
 import classes from './Header.module.scss';
 
-const version = '4.0.2';
+const version = '4.0.3';
 
 const isDarkMode =
   window.matchMedia &&
