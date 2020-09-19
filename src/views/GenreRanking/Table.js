b8cd4b339000ebc878212bcd7ced686b763faa78
André Ascensão <andremiguelaa@gmail.com>
2020-09-19 19:01:10 +0100

    genre ranking improvements

--- src/views/GenreRanking/Table.js
+++ src/views/GenreRanking/Table.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { Trans } from '@lingui/macro';
+import { t, Trans } from '@lingui/macro';
 import { I18n } from '@lingui/react';
-import { t } from '@lingui/macro';
 import classnames from 'classnames';
-
+import React from 'react';
 import { useStateValue } from 'state/State';
-
 import classes from './Table.module.scss';
 
 const Table = ({ data, users, genre }) => {
   const [{ user }] = useStateValue();
+  let rank = 0;
   return (
     <table className="table is-fullwidth is-hoverable">
       <thead>
         <tr>
+          <th>#</th>
           <th>
             <Trans>Nome</Trans>
           </th>
@@ -56,33 +55,51 @@ const Table = ({ data, users, genre }) => {
         </tr>
       </thead>
       <tbody>
-        {data.map((player) => (
-          <tr
-            key={player.user}
-            className={classnames({
-              'has-background-success': player.user === user.id,
-            })}
-          >
-            <td>
-              {users[player.user].name} {users[player.user].surname}
-            </td>
-            <td className={classes.correct}>
-              {(genre ? player.genres[genre]?.correct : player.correct) || 0}
-            </td>
-            <td className={classes.total}>
-              {(genre ? player.genres[genre]?.total : player.total) || 0}
-            </td>
-            <td className={classes.percentage}>
-              {Math.round(
-                (genre
-                  ? (player.genres[genre]?.correct || 0) /
-                    (player.genres[genre]?.total || 1)
-                  : player.correct / (player.total || 1)) * 100
-              )}
-              %
-            </td>
-          </tr>
-        ))}
+        {data.map((player, index) => {
+          if (
+            !(
+              index &&
+              (genre
+                ? (player.genres[genre]?.correct || 0) * 10 +
+                    (player.genres[genre]?.total || 0) ===
+                  (data[index - 1].genres[genre]?.correct || 0) * 10 +
+                    (data[index - 1].genres[genre]?.total || 0)
+                : (player.correct || 0) * 10 + (player.total || 0) ===
+                  (data[index - 1].correct || 0) * 10 +
+                    (data[index - 1].total || 0))
+            )
+          ) {
+            rank = index + 1;
+          }
+          return (
+            <tr
+              key={player.user}
+              className={classnames({
+                'has-background-success': player.user === user.id,
+              })}
+            >
+              <td className={classes.rank}>{rank}</td>
+              <td>
+                {users[player.user].name} {users[player.user].surname}
+              </td>
+              <td className={classes.correct}>
+                {(genre ? player.genres[genre]?.correct : player.correct) || 0}
+              </td>
+              <td className={classes.total}>
+                {(genre ? player.genres[genre]?.total : player.total) || 0}
+              </td>
+              <td className={classes.percentage}>
+                {Math.round(
+                  (genre
+                    ? (player.genres[genre]?.correct || 0) /
+                      (player.genres[genre]?.total || 1)
+                    : player.correct / (player.total || 1)) * 100
+                )}
+                %
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
