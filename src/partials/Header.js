ef8765bb5cc6332752f8b7a2ca3890060f285cb9
André Ascensão <andremiguelaa@gmail.com>
2020-12-14 00:23:51 +0000

    rules translation

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -13,7 +13,7 @@ import logo from 'img/logo.png';
 import logoDark from 'img/logo-dark.png';
 import classes from './Header.module.scss';
 
-const version = '4.12.4';
+const version = '4.12.5';
 
 const isDarkMode =
   window.matchMedia &&
