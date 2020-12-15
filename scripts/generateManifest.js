b228c6d72853c1defa5d5b69d949c2bd7ef4cbc6
André Ascensão <andremiguelaa@gmail.com>
2020-12-15 02:10:11 +0000

    favicons

--- scripts/generateManifest.js
+++ scripts/generateManifest.js
@@ -1,10 +1,16 @@
 require('dotenv').config({
   path: './.env',
-})
+});
 const replace = require('replace-in-file');
-const options = {
+
+replace({
   files: './public/manifest.json',
-  from: /%REACT_APP_NAME%/g,
-  to: process.env.REACT_APP_NAME,
-};
-replace(options);
+  from: [/%REACT_APP_NAME%/g, /%REACT_APP_SLUG%/g, /%REACT_APP_COLOR%/g],
+  to: [process.env.REACT_APP_NAME, process.env.REACT_APP_SLUG, process.env.REACT_APP_COLOR],
+});
+
+replace({
+  files: './public/browserconfig.xml',
+  from: [/%REACT_APP_SLUG%/g, /%REACT_APP_COLOR%/g],
+  to: [process.env.REACT_APP_SLUG, process.env.REACT_APP_COLOR],
+});
