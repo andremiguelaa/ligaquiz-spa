cd2bf6a7255a7d2ce87c153bdb210a7986e7d88c
André Ascensão <andremiguelaa@gmail.com>
2021-01-01 16:47:08 +0000

    hide answer if quiz not played

--- src/views/Quiz/QuizDone.js
+++ src/views/Quiz/QuizDone.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Trans } from '@lingui/macro';
 import classnames from 'classnames';
@@ -8,28 +8,50 @@ import Markdown from 'components/Markdown';
 
 import classes from './Quiz.module.scss';
 
-const QuizDone = ({ data, userAnswers }) => (
-  <>
-    {data.quiz.questions.map((question, index) => (
-      <div key={question.id} className={classes.question}>
-        <h2 className="subtitle has-text-weight-bold">
-          <Trans>Pergunta {index + 1}</Trans>
-        </h2>
-        {question.content && (
-          <div className={classes.questionText}>
-            <Markdown content={question.content} />
-          </div>
-        )}
-        {question.answer && (
-          <div>
-            <strong>
-              <Trans>Resposta correcta</Trans>:
-            </strong>{' '}
-            {question.answer}
-          </div>
-        )}
-        {userAnswers?.[question.id]?.[0] &&
-          Boolean(userAnswers?.[question.id]?.[0].submitted) && (
+const QuizDone = ({ data, userAnswers }) => {
+  const [questions, setQuestions] = useState(
+    data.quiz.questions.map((question) => ({
+      ...question,
+      userHasAnswered: Boolean(userAnswers?.[question.id]?.[0]?.submitted),
+      showAnswer: Boolean(userAnswers?.[question.id]?.[0]?.submitted),
+    }))
+  );
+  return (
+    <>
+      {questions.map((question, index) => (
+        <div key={question.id} className={classes.question}>
+          <h2 className="subtitle has-text-weight-bold">
+            <Trans>Pergunta {index + 1}</Trans>
+          </h2>
+          {question.content && (
+            <div className={classes.questionText}>
+              <Markdown content={question.content} />
+            </div>
+          )}
+          {question.answer && (
+            <div>
+              <strong>
+                <Trans>Resposta correcta</Trans>:
+              </strong>{' '}
+              {question.showAnswer ? (
+                question.answer
+              ) : (
+                <button
+                  className="is-link"
+                  onClick={() => {
+                    setQuestions((prev) => {
+                      const newQuestions = [...prev];
+                      newQuestions[index].showAnswer = true;
+                      return newQuestions;
+                    });
+                  }}
+                >
+                  <Trans>Mostrar resposta</Trans>
+                </button>
+              )}
+            </div>
+          )}
+          {question.userHasAnswered && (
             <div>
               <strong>
                 <Trans>Resposta dada</Trans>:
@@ -39,49 +61,50 @@ const QuizDone = ({ data, userAnswers }) => (
                 : '-'}
             </div>
           )}
-        {!data.quiz.solo && userAnswers?.[question.id]?.[0] && (
-          <div>
-            <strong>
-              <Trans>Pontos atribuídos ao adversário da Liga</Trans>:
-            </strong>{' '}
-            {userAnswers[question.id][0].points}
-          </div>
-        )}
-        {data.quiz.cupOpponent && userAnswers?.[question.id]?.[0] && (
-          <div>
-            <strong>
-              <Trans>Pontos atribuídos ao adversário da Taça</Trans>:
-            </strong>{' '}
-            {userAnswers[question.id][0].cup_points}
-          </div>
-        )}
-        {question.hasOwnProperty('percentage') && (
-          <div>
-            <strong>
-              <Trans>Percentagem de acerto</Trans>:
-            </strong>{' '}
-            <Link to={`/question/${question.id}`}>
-              {Math.round(question.percentage)}%
-            </Link>
-          </div>
-        )}
-        {question.media_id && (
-          <div
-            className={classnames(
-              classes.media,
-              classes[data.media[question.media_id].type]
-            )}
-          >
-            {renderMedia(
-              data.media[question.media_id].type,
-              data.media[question.media_id].url,
-              index
-            )}
-          </div>
-        )}
-      </div>
-    ))}
-  </>
-);
+          {!data.quiz.solo && userAnswers?.[question.id]?.[0] && (
+            <div>
+              <strong>
+                <Trans>Pontos atribuídos ao adversário da Liga</Trans>:
+              </strong>{' '}
+              {userAnswers[question.id][0].points}
+            </div>
+          )}
+          {data.quiz.cupOpponent && userAnswers?.[question.id]?.[0] && (
+            <div>
+              <strong>
+                <Trans>Pontos atribuídos ao adversário da Taça</Trans>:
+              </strong>{' '}
+              {userAnswers[question.id][0].cup_points}
+            </div>
+          )}
+          {question.hasOwnProperty('percentage') && (
+            <div>
+              <strong>
+                <Trans>Percentagem de acerto</Trans>:
+              </strong>{' '}
+              <Link to={`/question/${question.id}`}>
+                {Math.round(question.percentage)}%
+              </Link>
+            </div>
+          )}
+          {question.media_id && (
+            <div
+              className={classnames(
+                classes.media,
+                classes[data.media[question.media_id].type]
+              )}
+            >
+              {renderMedia(
+                data.media[question.media_id].type,
+                data.media[question.media_id].url,
+                index
+              )}
+            </div>
+          )}
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default QuizDone;
