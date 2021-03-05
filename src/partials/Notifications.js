3b1b0ce3aa4d21cd2f51b9c68bd54cb586608c18
André Ascensão <andremiguelaa@gmail.com>
2021-03-05 01:29:22 +0000

    dismissable notifications

--- src/partials/Notifications.js
+++ src/partials/Notifications.js
@@ -4,11 +4,15 @@ import { Trans } from '@lingui/macro';
 import { I18n } from '@lingui/react';
 import { t } from '@lingui/macro';
 import { differenceInDays } from 'date-fns';
+import Cookies from 'js-cookie';
+import classnames from 'classnames';
 
 import Markdown from 'components/Markdown';
 import { useStateValue } from 'state/State';
 import ApiRequest from 'utils/ApiRequest';
 import Loading from 'components/Loading';
+
+import classes from './Notifications.module.scss';
 
 const Notifications = () => {
   const location = useLocation();
@@ -120,6 +124,36 @@ const Notifications = () => {
     }
   }, [notifications.special_quiz]);
 
+  const [
+    dismissedNotificationsSlugs,
+    setDismissedNotificationsSlugs,
+  ] = useState(
+    Cookies.get('dismissed-notifications')
+      ? JSON.parse(Cookies.get('dismissed-notifications'))
+      : []
+  );
+
+  const showDimissedNotifications = () => {
+    setDismissedNotificationsSlugs([]);
+    Cookies.set('dismissed-notifications', [], {
+      expires: 365,
+      sameSite: 'strict',
+    });
+  };
+
+  const dismissNotification = (slug) => {
+    let newCookieValue;
+    newCookieValue = Cookies.get('dismissed-notifications')
+      ? JSON.parse(Cookies.get('dismissed-notifications'))
+      : [];
+    newCookieValue.push(slug);
+    setDismissedNotificationsSlugs(newCookieValue);
+    Cookies.set('dismissed-notifications', [...new Set(newCookieValue)], {
+      expires: 365,
+      sameSite: 'strict',
+    });
+  };
+
   if (notifications.loading) {
     return <Loading />;
   }
@@ -153,6 +187,32 @@ const Notifications = () => {
     notifications.not_corrected_quizzes ||
     notifications.not_corrected_special_quizzes
   ) {
+    const {
+      customNotifications,
+      dismissedNotifications,
+    } = notifications.data.reduce(
+      (acc, item) => {
+        if (!dismissedNotificationsSlugs.includes(`custom-${item.id}`)) {
+          acc.customNotifications.push(item);
+        } else {
+          acc.dismissedNotifications.push(item);
+        }
+        return acc;
+      },
+      { customNotifications: [], dismissedNotifications: [] }
+    );
+
+    const dismissedSpecialQuizNotification =
+      notifications.special_quiz_yesterday?.winners?.length > 0 &&
+      dismissedNotificationsSlugs.includes(
+        `special-quiz-${notifications.special_quiz_yesterday.date}`
+      );
+
+    let dismissedNotificationsCount = dismissedNotifications.length;
+    if (dismissedSpecialQuizNotification) {
+      dismissedNotificationsCount++;
+    }
+
     return (
       <section className="section">
         {user && user.roles === null && (
@@ -163,11 +223,38 @@ const Notifications = () => {
             </Trans>
           </div>
         )}
-        {notifications.data.map((notification) => (
+        {dismissedNotificationsCount > 0 && (
+          <div className="is-clearfix">
+            <button
+              className={classnames(
+                'button',
+                'is-small',
+                'is-warning',
+                'is-pulled-right',
+                classes.showDismissedButton
+              )}
+              onClick={() => showDimissedNotifications()}
+            >
+              {dismissedNotificationsCount === 1 ? (
+                <Trans>Mostrar notificação oculta</Trans>
+              ) : (
+                <>
+                  <Trans>Mostrar notificações ocultas</Trans> (
+                  {dismissedNotificationsCount})
+                </>
+              )}
+            </button>
+          </div>
+        )}
+        {customNotifications.map((notification) => (
           <div
             key={notification.id}
             className={`notification is-${notification.type}`}
           >
+            <button
+              className="delete"
+              onClick={() => dismissNotification(`custom-${notification.id}`)}
+            ></button>
             <Markdown content={notification.content} />
           </div>
         ))}
@@ -229,40 +316,51 @@ const Notifications = () => {
             </Trans>
           </div>
         )}
-        {notifications.special_quiz_yesterday?.winners?.length > 0 && (
-          <div className={`notification is-warning`}>
-            {notifications.special_quiz_yesterday.winners.length > 1 ? (
-              <I18n>
-                {({ i18n }) => (
-                  <Trans>
-                    A vitória no quiz especial{' '}
-                    <Link
-                      to={`/special-quiz/${notifications.special_quiz_yesterday.date}`}
-                    >
-                      <em>{notifications.special_quiz_yesterday.subject}</em>
-                    </Link>{' '}
-                    é d@
-                    {specialQuizWinners.slice(0, -1).join(i18n._(t`, d@`)) +
-                      ' ' +
-                      i18n._(t`e d@`) +
-                      ' ' +
-                      specialQuizWinners.slice(-1)}. Parabéns!
-                  </Trans>
-                )}
-              </I18n>
-            ) : (
-              <Trans>
-                A vitória no quiz especial{' '}
-                <Link
-                  to={`/special-quiz/${notifications.special_quiz_yesterday.date}`}
-                >
-                  <em>{notifications.special_quiz_yesterday.subject}</em>
-                </Link>{' '}
-                é d@ {specialQuizWinners[0]}. Parabéns!
-              </Trans>
-            )}
-          </div>
-        )}
+        {notifications.special_quiz_yesterday?.winners?.length > 0 &&
+          !dismissedNotificationsSlugs.includes(
+            `special-quiz-${notifications.special_quiz_yesterday.date}`
+          ) && (
+            <div className={`notification is-warning`}>
+              <button
+                className="delete"
+                onClick={() =>
+                  dismissNotification(
+                    `special-quiz-${notifications.special_quiz_yesterday.date}`
+                  )
+                }
+              ></button>
+              {notifications.special_quiz_yesterday.winners.length > 1 ? (
+                <I18n>
+                  {({ i18n }) => (
+                    <Trans>
+                      A vitória no quiz especial{' '}
+                      <Link
+                        to={`/special-quiz/${notifications.special_quiz_yesterday.date}`}
+                      >
+                        <em>{notifications.special_quiz_yesterday.subject}</em>
+                      </Link>{' '}
+                      é d@
+                      {specialQuizWinners.slice(0, -1).join(i18n._(t`, d@`)) +
+                        ' ' +
+                        i18n._(t`e d@`) +
+                        ' ' +
+                        specialQuizWinners.slice(-1)}. Parabéns!
+                    </Trans>
+                  )}
+                </I18n>
+              ) : (
+                <Trans>
+                  A vitória no quiz especial{' '}
+                  <Link
+                    to={`/special-quiz/${notifications.special_quiz_yesterday.date}`}
+                  >
+                    <em>{notifications.special_quiz_yesterday.subject}</em>
+                  </Link>{' '}
+                  é d@ {specialQuizWinners[0]}. Parabéns!
+                </Trans>
+              )}
+            </div>
+          )}
         {notifications.quiz && location.pathname !== '/quiz' && (
           <div className={`notification is-info`}>
             <Link to="/quiz">
